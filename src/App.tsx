0ff9ddc69a415d98631b32b0700f61970b81d85d
zangxiaohui <12524051@qq.com>
2023-09-01 16:22:10 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.scss";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
 const App: React.FC = () => {
@@ -9,22 +8,14 @@ const App: React.FC = () => {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/client/login" exact component={Login} />
-          <Route path="/client/register" exact component={Login} />
-          <Route
-            exact
-            path="/"
-            render={() => <Redirect to="/client/login" />}
-          />
-
-          <Route path="/">
+          <Route path="/client">
             <Switch>
-              {/* <Route
-                  exact
-                  path="/"
-                  render={() => <Redirect to="/dashboard" />}
-                /> */}
-              <Route path="/client/dashboard" exact component={Dashboard} />
+              <Route path="/client/login" exact component={Login} />
+              <Route
+                exact
+                path="/client"
+                render={() => <Redirect to="/client/login" />}
+              />
             </Switch>
           </Route>
         </Switch>
