8b89d0c37249b1e4aa2f4c57d6853f0b79f66ee9
臧小慧 <jane.zang@factchina.com>
2023-09-04 13:03:59 +0800

    wip

--- src/pages/Bid/Detail/index.tsx
+++ src/pages/Bid/Detail/index.tsx
@@ -1,18 +1,13 @@
-import { Button, Col, Form, InputNumber, Row, Tabs } from "antd";
+import { Button, Col, Form, InputNumber, Row, Statistic, Tabs } from "antd";
 import React from "react";
 import PageContainer from "../../../components/PageContainer";
 import "./index.less";
 
-const data = [
-  {
-    title: "这里显示产品名称，这里显示产品名称，100吨",
-  },
-  {
-    title: "这里显示产品名称，这里显示产品名称，100吨",
-  },
-];
+const { Countdown } = Statistic;
 
 const Home: React.FC = () => {
+  const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
+
   const routes = [
     {
       breadcrumbName: "首页",
@@ -30,7 +25,7 @@ const Home: React.FC = () => {
   };
 
   const items = [
-    { label: "拍品详情", key: "item-1", children: "内容 1" },
+    { label: "拍品详情", key: "item-1", children: "内容 1 ￥" },
     { label: "招标公告", key: "item-2", children: "内容 2" },
   ];
 
@@ -40,15 +35,30 @@ const Home: React.FC = () => {
 
   return (
     <PageContainer routes={routes}>
+      <div className="bid-row1">
+        <h1>这里显示产品名称，这里显示产品名称，100吨</h1>
+        <div className="statistic-wrap">
+          <Countdown
+            title="距结束"
+            value={deadline}
+            format="D 天 H 时 m 分 s 秒"
+          />
+          <Statistic title="当前价" value={9999999} prefix="￥" />
+        </div>
+      </div>
       <Row className="bid-row2">
         <Col flex="540px">
           <Form labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
             <Form.Item label="出&nbsp;&nbsp;价">
               <InputNumber
                 min={1}
-                defaultValue={3}
+                defaultValue={9992929}
                 onChange={onChange2}
                 size="large"
+                formatter={(value) =>
+                  `￥${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                }
+                parser={(value) => value!.replace(/\$\s?|(,*)/g, "") as any}
               />
             </Form.Item>
             <Form.Item wrapperCol={{ offset: 6, span: 18 }}>
